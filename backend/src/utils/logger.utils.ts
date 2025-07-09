54a1aaab617cf56495493474f976e563ccc6d6df
Alex Faustino <faustino.alex@outlook.com>
2025-07-09 19:47:27 -0300

    feat: refactor logger utility to use custom formatting and improve timestamp handling

--- backend/src/utils/logger.utils.ts
+++ backend/src/utils/logger.utils.ts
@@ -1,18 +1,54 @@
-import { createLogger, format, transports } from "winston";
+import { createLogger, format, type Logger, transports } from "winston";
 
-export const logger = createLogger({
-  level: "info",
-  format: format.combine(
-    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json(),
-  ),
-  transports: [
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    }),
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
-  ],
-});
+function buildCustomFormat() {
+  return format.printf(({ timestamp, level, message, ...meta }) => {
+    const dataKey = meta.data ? "data" : meta.log_data ? "log_data" : null;
+    const dataValue = dataKey ? JSON.stringify(meta[dataKey]) : "";
+    if (dataKey) delete meta[dataKey];
+
+    const metaString = Object.keys(meta).length
+      ? `| ${JSON.stringify(meta)}`
+      : "";
+
+    return [
+      timestamp,
+      level.toUpperCase(),
+      "|",
+      message,
+      dataKey ? `| ${dataKey}:${dataValue}` : "",
+      metaString,
+    ]
+      .filter(Boolean)
+      .join(" ");
+  });
+}
+
+function buildLogger(): Logger {
+  const logLevel = process.env.LOG_LEVEL || "info";
+  const timestampFormat = "ddd, DD MMM YYYY HH:mm:ss";
+
+  return createLogger({
+    level: logLevel,
+    format: format.combine(
+      format.timestamp({ format: timestampFormat }),
+      format.errors({ stack: true }),
+      format.splat(),
+      buildCustomFormat(),
+    ),
+    transports: [
+      new transports.Console({
+        level: logLevel,
+        format: format.combine(
+          format.timestamp({ format: timestampFormat }),
+          format.splat(),
+          buildCustomFormat(),
+        ),
+      }),
+      new transports.File({ filename: "logs/error.log", level: "error" }),
+      new transports.File({ filename: "logs/combined.log" }),
+    ],
+    exitOnError: false,
+  });
+}
+
+export const logger = buildLogger();
