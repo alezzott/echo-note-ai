38981ac836ca10f152e3d2d60adfcf80c7383a6c
Alex Faustino <faustino.alex@outlook.com>
2025-07-12 12:04:40 -0300

    refactor: move audio transcription logic to useCreateTranscription composable

--- frontend/src/components/transcriptions/CreateTranscription.vue
+++ frontend/src/components/transcriptions/CreateTranscription.vue
@@ -1,71 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useUserStore } from "../../stores/user";
-import { useLoading } from "../../composables/useLoading";
-import { Progress } from "../ui/progress";
-import { toast } from "vue-sonner";
-import { transcribeAudio } from "../../api/create-transcription";
 import AudioUpload from "./AudioUpload.vue";
 import TranscriptionResult from "./TranscriptionResult.vue";
-import axios, { type CancelTokenSource } from "axios";
 import AudioRecorder from "./AudioRecorder.vue";
 import { Loader2 } from "lucide-vue-next";
+import { Progress } from "../ui/progress";
+import { useCreateTranscription } from "@/composables/useCreateTranscription";
 
-const userStore = useUserStore();
-const { loading, start, stop } = useLoading();
-
-const audioFile = ref<File | null>(null);
-const transcript = ref("");
-const error = ref("");
-const progress = ref(0);
-const cancelToken = ref<CancelTokenSource | null>(null);
-
-async function handleUpload() {
-  if (!audioFile.value) return;
-  start();
-  error.value = "";
-  transcript.value = "";
-  progress.value = 0;
-  cancelToken.value = axios.CancelToken.source();
-
-  try {
-    const response = await transcribeAudio({
-      audioFile: audioFile.value,
-      token: userStore.token ?? "",
-      cancelToken: cancelToken.value,
-      onUploadProgress: (evt) => {
-        if (evt.total) {
-          progress.value = Math.round((evt.loaded / evt.total) * 100);
-        }
-      },
-    });
-    transcript.value = response.text;
-    toast.success("Audio enviado com sucesso !");
-  } catch (err: any) {
-    if (axios.isCancel(err)) {
-      toast.error("Envio cancelado pelo usuário.");
-    } else {
-      error.value = err?.response?.data?.error || "Erro ao transcrever áudio";
-      toast.error("Erro ao enviar áudio, tente novamente");
-    }
-  } finally {
-    stop();
-    progress.value = 0;
-    cancelToken.value = null;
-  }
-}
-
-function handleFileChange(file: File) {
-  audioFile.value = file;
-  error.value = "";
-}
-
-function cancelUpload() {
-  if (cancelToken.value) {
-    cancelToken.value.cancel("Upload cancelado pelo usuário.");
-    audioFile.value = null;
-  }
-}
+const {
+    loading,
+    audioFile,
+    transcript,
+    error,
+    progress,
+    handleUpload,
+    handleFileChange,
+    cancelUpload,
+} = useCreateTranscription();
 </script>
 
 <template>
