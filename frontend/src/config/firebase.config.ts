a7f0e81ff5854664208177eb7452c54123cf53ac
Alex Faustino <faustino.alex@outlook.com>
2025-07-12 01:23:08 -0300

    feat: add Firebase configuration variables to .env.example and update firebase.config.ts to use environment variables

--- frontend/src/config/firebase.config.ts
+++ frontend/src/config/firebase.config.ts
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCF_tXZggPYztD0Ld2TTYjR7FuYICqieSk",
-	authDomain: "echo-note-ai.firebaseapp.com",
-	projectId: "echo-note-ai",
-	appId: "1:58222853866:web:e8d1a4072fffb78a4aa193",
+	apiKey: import.meta.env.FIREBASE_API_KEY,
+	authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.FIREBASE_PROJECT_ID,
+	appId: import.meta.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
