88dd5596a08a81013bf6e6650b21b9ed8318f016
Alex Faustino <faustino.alex@outlook.com>
2025-07-21 14:23:24 -0300

    ref: format txt export

--- backend/src/utils/export.utils.ts
+++ backend/src/utils/export.utils.ts
@@ -1,15 +1,19 @@
 import type { ITranscription } from "../models/transcription.model";
 
-export function exportToTxt(transcription: ITranscription) {
-  let content = `Transcrição: ${transcription.transcript}\n\n`;
-  if (transcription.segments?.length) {
-    content += transcription.segments
-      .map((seg, _i) => `[${seg.start}s - ${seg.end}s] ${seg.text}`)
-      .join("\n");
-  }
+export function exportManyToTxt(transcriptions: ITranscription[], userId: string) {
+  let content = "";
+  transcriptions.forEach((t, idx) => {
+    content += `Transcrição #${idx + 1} (${t.filename || t._id}):\n${t.transcript}\n\n`;
+    if (t.segments?.length) {
+      content += t.segments
+        .map((seg) => `[${seg.start}s - ${seg.end}s] ${seg.text}`)
+        .join("\n");
+      content += "\n\n";
+    }
+  });
   return {
     buffer: Buffer.from(content, "utf-8"),
-    filename: `transcription-${transcription._id}.txt`,
+    filename: `transcriptions-${userId}.txt`,
     mimeType: "text/plain",
   };
 }
