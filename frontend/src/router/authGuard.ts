c99af2fab40a381a8a8845b23d12ea077920f35a
Alex Faustino <faustino.alex@outlook.com>
2025-07-22 12:41:25 -0300

    refactor: adjust formatting and improve error messages in authGuard

--- frontend/src/router/authGuard.ts
+++ frontend/src/router/authGuard.ts
@@ -1,26 +1,28 @@
-import type { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
-import { isTokenExpired } from "../helpers/expires-token";
-import { useUserStore } from "../stores/user";
+import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+import { isTokenExpired } from '../helpers/expires-token';
+import { useUserStore } from '../stores/user';
+import { toast } from 'vue-sonner';
 
-const publicPages = ["/login"];
+const publicPages = ['/login'];
 
 export function authGuard(
-	to: RouteLocationNormalized,
-	_from: RouteLocationNormalized,
-	next: NavigationGuardNext,
+  to: RouteLocationNormalized,
+  _from: RouteLocationNormalized,
+  next: NavigationGuardNext,
 ): void {
-	const userStore = useUserStore();
-	const isAuth =
-		!!userStore.user && !!userStore.token && !isTokenExpired(userStore.token);
+  const userStore = useUserStore();
+  const isAuth =
+    !!userStore.user && !!userStore.token && !isTokenExpired(userStore.token);
 
-	if (!isAuth && !publicPages.includes(to.path)) {
-		userStore.clearUser();
-		next("/login");
-	}
-	// Se está autenticado e acessa /login, redireciona para dashboard
-	else if (isAuth && to.path === "/login") {
-		next("/dashboard");
-	} else {
-		next();
-	}
+  if (!isAuth && !publicPages.includes(to.path)) {
+    userStore.clearUser();
+    toast.error('Sessão expirada. Faça login novamente.');
+    next('/');
+  }
+  // Se está autenticado e acessa /login, redireciona para dashboard
+  else if (isAuth && to.path === '/login') {
+    next('/dashboard');
+  } else {
+    next();
+  }
 }
