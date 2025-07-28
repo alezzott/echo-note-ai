e44da1f4e3f811b558da092dda5119d65f0c447b
Alex Faustino <faustino.alex@outlook.com>
2025-07-28 19:06:26 -0300

    feat: update tooltip components and dependencies

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <Toaster richColors position="top-right" :duration="5000" />
-  <RouterView />
+  <TooltipProvider>
+    <Toaster richColors position="top-right" :duration="5000" />
+    <RouterView />
+  </TooltipProvider>
 </template>
 
 <script setup lang="ts">
 import { Toaster } from 'vue-sonner';
 import 'vue-sonner/style.css';
+import TooltipProvider from './components/ui/tooltip/TooltipProvider.vue';
 </script>
