c67a24df4c8b12aff1710c80fe3aa39822aafbfa
Alex Faustino <faustino.alex@outlook.com>
2025-07-28 19:07:19 -0300

    refactor: ajustar a formatação do componente LoginButton

--- frontend/src/components/login/LoginButton.vue
+++ frontend/src/components/login/LoginButton.vue
@@ -1,29 +1,35 @@
 <script setup lang="ts">
-import { Loader2 } from "lucide-vue-next";
-import GoogleIcon from "../../assets/login/google-logo.svg";
-import Button from "../ui/button/Button.vue";
+import { Loader2 } from 'lucide-vue-next';
+import GoogleIcon from '../../assets/login/google-logo.svg';
+import Button from '../ui/button/Button.vue';
 
 defineProps<{
-	loading: boolean;
-	onClick: () => void;
+  loading: boolean;
+  onClick: () => void;
 }>();
 </script>
 
 <template>
-    <Button :disabled="loading" aria-label="Entrar com Google"
-        class="w-64 h-11 m-auto flex items-center justify-center gap-3 border-red-500 rounded-[20px] font-medium text-[#1f1f1f] text-lg"
-        @click="onClick" variant="outline">
-        <span class="flex items-center justify-center h-5 mr-3 min-w-[20px]">
-            <template v-if="loading">
-                <Loader2 class="w-6 h-6 mr-2 animate-spin" />
-            </template>
-            <template v-else>
-                <img :src="GoogleIcon" alt="Google" class="w-5 h-5" draggable="false" />
-            </template>
-        </span>
-        <span class="flex-grow text-ellipsis overflow-hidden text-center font-semibold cursor-pointer text-red-400">
-            <span v-if="loading">Aguarde...</span>
-            <span v-else>Entrar com Google</span>
-        </span>
-    </Button>
+  <Button
+    :disabled="loading"
+    aria-label="Entrar com Google"
+    class="w-64 h-11 m-auto flex items-center justify-center gap-3 border-red-500 cursor-pointer rounded-md font-medium text-[#1f1f1f] text-lg hover:bg-red-50"
+    @click="onClick"
+    variant="outline"
+  >
+    <span class="flex items-center justify-center h-5 mr-3 min-w-[20px]">
+      <template v-if="loading">
+        <Loader2 class="w-6 h-6 mr-2 animate-spin" />
+      </template>
+      <template v-else>
+        <img :src="GoogleIcon" alt="Google" class="w-5 h-5" draggable="false" />
+      </template>
+    </span>
+    <span
+      class="flex-grow text-ellipsis overflow-hidden text-center font-semibold cursor-pointer text-red-400"
+    >
+      <span v-if="loading">Aguarde...</span>
+      <span v-else>Entrar com Google</span>
+    </span>
+  </Button>
 </template>
---
+++ frontend/src/components/transcriptions/TooltipAudioInfo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { Info } from 'lucide-vue-next';
+</script>
+
+<template>
+  <Tooltip>
+    <TooltipTrigger as-child>
+      <button class="cursor-pointer">
+        <Info :size="18" class="hover:text-orange-400" />
+      </button>
+    </TooltipTrigger>
+    <TooltipContent>
+      <span>
+        Formatos suportados: WAV, MPEG, MP4, AAC, M4A, X-M4A.<br />
+        Outros formatos não serão aceitos no upload.
+      </span>
+    </TooltipContent>
+  </Tooltip>
+</template>
