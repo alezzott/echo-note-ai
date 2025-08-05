dd68078f4dc51d057ec953d0ab686ac0913cd494
Alex Faustino <faustino.alex@outlook.com>
2025-08-05 18:57:17 -0300

    ref:login and style for consistency and styling improvements

--- frontend/src/components/login/LoginHeader.vue
+++ frontend/src/components/login/LoginHeader.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import VoiceIllustrator from "../../assets/login/voice-illustrator.svg";
+import VoiceIllustrator from '../../assets/login/voice-illustrator.svg';
 </script>
 
 <template>
-  <section class="flex-1/3 flex flex-col items-center justify-center bg-[#fb923c] p-8">
+  <section
+    class="flex-1/3 flex flex-col items-center justify-center bg-[#fb923c] p-8"
+  >
     <img
       :src="VoiceIllustrator"
       alt="Ilustração"
